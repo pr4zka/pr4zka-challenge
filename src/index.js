a14e943334dfbf30adf9092a69c23aa276fd6aaa
ivan <ivanhakate1028@gmail.com>
2022-11-05 14:06:44 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -10,20 +10,20 @@ const swaggerJsDoc = require("swagger-jsdoc");
 
 //swagger documentation
 const swaggerSpec = {
-  definition:{
+  definition: {
     openapi: "3.0.0",
-    info:{
+    info: {
       title: "API Dinesy - Challenge Alkemy",
       version: "1.0.0",
     },
     servers: [
       {
         url: "http://localhost:3000/",
-      }
-    ]
+      },
+    ],
   },
-  apis: [`${path.join(__dirname, "./routes/*.js")}`]
-}
+  apis: [`${path.join(__dirname, "./routes/*.js")}`],
+};
 
 //middleware
 app.use(express.urlencoded({ extended: false }));
@@ -31,8 +31,12 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
-//para leer la documentacion de la API  
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
+//para leer la documentacion de la API
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerJsDoc(swaggerSpec))
+);
 
 //config
 const port = process.env.PORT || 3000;
@@ -44,13 +48,13 @@ app.use("/api/", require("./routes/peliculas"));
 app.use("/api/", require("./routes/genero"));
 app.get("/", (req, res) => {
   res.redirect("/api-docs");
-})
-
+});
 
 //server
-app.listen(port, () => { 
-  sequelize.sync({ force: true });
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  sequelize.sync({ force: false }).then(() => {
+    console.log(`Server running on port ${port}`);
+  });
 });
 
 dbConnectMysql();
