2d0221e0cfb8d7f626f90401659d16360183d882
ivan <ivanhakate1028@gmail.com>
2022-11-05 14:20:12 -0300

    db

--- src/database/db.js
+++ src/database/db.js
@@ -4,19 +4,22 @@ const database = process.env.DATABASE;
 const username = process.env.USER;
 const password = process.env.PASSWORD;
 const host = process.env.HOST;
-
+const port = process.env.PORT_DB;
 const sequelize = new Sequelize(database, username, password, {
   host,
+  port,
   dialect: "mysql",
 });
 
 const dbConnectMysql = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Mysql database is connected");
-  } catch (error) {
-    console.log("Mysql error de conexion", error);
-  }
+  await sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Connection has been established successfully.");
+    })
+    .catch((err) => {
+      console.log("Unable to connect to the database:", err);
+    });
 };
 
 module.exports = {
