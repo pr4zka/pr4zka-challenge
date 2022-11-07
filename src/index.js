c073c110adfeea8947603f170430a3395c89d3c5
ivan <ivanhakate1028@gmail.com>
2022-11-07 02:27:25 -0300

    fix rutas

--- src/index.js
+++ src/index.js
@@ -47,31 +47,36 @@ app.use(
 const port = process.env.PORT || 3000;
 
 //routes
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+app.all("*", (req, res) => {
+  res.redirect("/api-docs");
+});
 app.use("/api/", require("./routes/personajes"));
 app.use("/api/auth/", require("./routes/auth"));
 app.use("/api/", require("./routes/peliculas"));
 app.use("/api/", require("./routes/genero"));
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-app.get("/", (req, res) => {
-  res.redirect("/api-docs");
-});
 
 //server
 app.listen(port, () => {
